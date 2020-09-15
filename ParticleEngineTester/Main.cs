666327de9699ad43026e11f0133d43f35e06ae99
Calvin Wilkinson <kinsondigital@gmail.com>
2020-09-15 08:58:47 -0400

    #2188 - Create new project

---
+++ ParticleEngineTester/Main.cs
@@ -0,0 +1,56 @@
+﻿// <copyright file="Main.cs" company="KinsonDigital">
+// Copyright (c) KinsonDigital. All rights reserved.
+// </copyright>
+
+namespace ParticleEngineTester
+{
+    using System;
+    using Microsoft.Xna.Framework;
+    using Microsoft.Xna.Framework.Graphics;
+
+    public class Main : Game
+    {
+        private readonly GraphicsDeviceManager graphics;
+        private SpriteBatch spriteBatch;
+        private SpriteFont fps;
+
+        public Main()
+        {
+            this.graphics = new GraphicsDeviceManager(this);
+            Content.RootDirectory = "Content";
+            IsMouseVisible = true;
+            IsFixedTimeStep = true;
+
+            TargetElapsedTime = TimeSpan.FromSeconds(1d / 60d);
+
+            this.graphics.PreparingDeviceSettings += (sender, e) =>
+            {
+                e.GraphicsDeviceInformation.PresentationParameters.PresentationInterval = PresentInterval.Immediate;
+            };
+        }
+
+        protected override void Initialize()
+        {
+            GraphicsDevice.BlendState = BlendState.AlphaBlend;
+            Window.Title = "Particle Engine Tester";
+            base.Initialize();
+        }
+
+        protected override void LoadContent()
+        {
+            this.spriteBatch = new SpriteBatch(GraphicsDevice);
+        }
+
+        protected override void Update(GameTime gameTime)
+        {
+            base.Update(gameTime);
+        }
+
+        protected override void Draw(GameTime gameTime)
+        {
+            GraphicsDevice.Clear(Color.CornflowerBlue);
+
+            base.Draw(gameTime);
+        }
+    }
+}
---
+++ ParticleEngineTester/Program.cs
@@ -0,0 +1,22 @@
+﻿// <copyright file="Program.cs" company="KinsonDigital">
+// Copyright (c) KinsonDigital. All rights reserved.
+// </copyright>
+
+namespace ParticleEngineTester
+{
+    using System;
+
+    /// <summary>
+    /// Runs the application.
+    /// </summary>
+    public static class Program
+    {
+        [STAThread]
+        private static void Main()
+        {
+            var main = new Main();
+
+            main.Run();
+        }
+    }
+}
