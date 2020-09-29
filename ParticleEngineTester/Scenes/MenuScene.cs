6e4b63713ce90c55e84f4944f1edc9df5b06fbbd
Calvin Wilkinson <kinsondigital@gmail.com>
2020-09-29 09:01:37 -0400

    #2249 - Update location of menu to be centered in scene

--- ParticleEngineTester/Scenes/MenuScene.cs
+++ ParticleEngineTester/Scenes/MenuScene.cs
@@ -48,7 +48,8 @@ namespace ParticleEngineTester.Scenes
                 this.menu.Add(sceneKey, sceneKey.ToTitle(), this.defaultColor);
             }
 
-            this.menu.Location = new Vector2(SceneCenter.X - (this.menu.Width / 2), SceneCenter.Y - (this.menu.Height / 2));
+            UpdateMenuLocation();
+
             this.menu.MouseEnter += Menu_MouseEnter;
 
             base.LoadContent();
@@ -57,6 +58,8 @@ namespace ParticleEngineTester.Scenes
         /// <inheritdoc/>
         public override void Update(GameTime gameTime)
         {
+            UpdateMenuLocation();
+
             this.menu.Update(gameTime);
 
             base.Update(gameTime);
@@ -91,5 +94,12 @@ namespace ParticleEngineTester.Scenes
         /// Invoked when a menu item has been clicked, and loads a scene.
         /// </summary>
         private void Menu_ItemClicked(object? sender, MenuItemClickedEventArgs e) => this.MenuClicked?.Invoke(this, e);
+
+        /// <summary>
+        /// Updates the location of the menu to the center of the scene.
+        /// </summary>
+        private void UpdateMenuLocation() => this.menu.Location = new Vector2(
+            SceneCenter.X - (this.menu.Width / 2),
+            SceneCenter.Y - (this.menu.Height / 2));
     }
 }
