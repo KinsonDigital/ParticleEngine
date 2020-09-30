4237d6e3b4e1a188ac904c3c68bebd9d71f0a60a
Calvin Wilkinson <kinsondigital@gmail.com>
2020-09-30 05:07:46 -0400

    #2223 - Add error logging to engine tester app

--- ParticleEngineTester/Program.cs
+++ ParticleEngineTester/Program.cs
@@ -5,7 +5,11 @@
 #pragma warning disable SA1200 // Using directives should be placed correctly
 using System;
 using System.Diagnostics.CodeAnalysis;
+using System.IO;
+using System.Reflection;
 using System.Runtime.CompilerServices;
+using Serilog;
+using Serilog.Core;
 #pragma warning restore SA1200 // Using directives should be placed correctly
 
 [assembly: InternalsVisibleTo(assemblyName: "ParticleEngineTesterTests", AllInternalsVisible = true)]
@@ -18,12 +22,42 @@ namespace ParticleEngineTester
     [ExcludeFromCodeCoverage]
     public static class Program
     {
+        private static readonly string AppPath = $@"{Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location)}\";
+        private static readonly string LogPath = $@"{AppPath}Logs\";
+
+        /// <summary>
+        /// Gets the error logger used to log errors to a file.
+        /// </summary>
+        public static Logger? ErrorLogger { get; private set; }
+
         [STAThread]
         private static void Main()
         {
-            var main = new Main();
+            SetupLogDirectory();
 
+            var shortDateTime = DateTime.Now.ToShortDateString().Replace('/', '-');
+
+            ErrorLogger = new LoggerConfiguration()
+                .WriteTo.File(
+                    $"{LogPath}error-logs-{shortDateTime}.txt",
+                    outputTemplate: "\n{Timestamp:HH:mm:ss} | {Message}\n\t{Exception}")
+                .CreateLogger();
+
+            var main = new Main();
             main.Run();
+        }
+
+        /// <summary>
+        /// Sets up the log directory where logs will be saved.
+        /// </summary>
+        private static void SetupLogDirectory()
+        {
+            if (Directory.Exists(LogPath))
+            {
+                return;
+            }
+
+            Directory.CreateDirectory(LogPath);
         }
     }
 }
